0485299f402310efad9e13d5bc2c9039523d6bda
mustfens0 <144704606+mustfens0@users.noreply.github.com>
2023-09-11 17:42:35 +0300

    time_till_deadline

---
+++ time-tial deadline.py
@@ -0,0 +1,13 @@
+import datetime
+
+user_input = input("enter your goal with a deadline separated by colon \n")
+input_list = user_input.split(":")
+
+goal = input_list[0]
+deadline = input_list[1]
+deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
+
+today_date = datetime.datetime.today()
+time_till = deadline_date - today_date
+hours_till = str(time_till.days)
+print(f"dear user time remaining for your goal : {goal} is {hours_till} days")
